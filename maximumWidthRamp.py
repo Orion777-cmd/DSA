fe8a72e4193d7af746479efb98fda06f9794c8b5
Orion777-cmd <89917090+Orion777-cmd@users.noreply.github.com>
2023-07-26 10:33:47 +0300

    Create maximumWidthRamp.py

---
+++ maximumWidthRamp.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        stack = []
+        for i in range(len(nums)):
+            if not stack or nums[stack[-1]] > nums[i]:
+                stack.append(i)
+        
+        res = 0
+        for j in range(len(nums)-1, -1 , -1):
+            while stack and nums[stack[-1]] <= nums[j]:
+                indice = stack.pop()
+                res = max(res, j - indice)
+        return res
