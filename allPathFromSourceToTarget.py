82132495fa4eac19e1591773149f348eaab7915a
Orion777-cmd <89917090+Orion777-cmd@users.noreply.github.com>
2023-07-30 19:56:22 +0300

    Update allPathFromSourceToTarget.py

--- allPathFromSourceToTarget.py
+++ allPathFromSourceToTarget.py
@@ -1,1 +1,29 @@
 
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        
+        def buildGraph(graph):
+            adjecent = {i:[] for i in range(len(graph))}
+            for i in range(len(graph)):
+                for j in graph[i]:
+                    adjecent[i].append(j)
+            return  adjecent
+
+        adjecent = buildGraph(graph)
+        # print(adjecent)
+
+        res = []
+        def dfs(node , path):
+            
+            if path[-1] == len(adjecent)-1:
+                res.append(path)
+                return
+            
+            for neighbor in adjecent[node]:
+                temp = path.copy()
+                temp.append(neighbor)
+                dfs(neighbor, temp)
+
+
+        dfs(0,[0])
+        return res
