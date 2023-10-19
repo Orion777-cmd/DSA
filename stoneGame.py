407794a6cae2a32f92cc1d8db58bc5b9cf810d8b
Orion777-cmd <89917090+Orion777-cmd@users.noreply.github.com>
2023-10-19 15:00:14 +0300

    Create stoneGame.py

---
+++ stoneGame.py
@@ -0,0 +1,19 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        n = len(piles)
+
+        @lru_cache(None)
+        def dp(i, j):
+            if i > j: 
+                return 0
+            
+            parity = (j-i-n) % 2
+            if parity == 1:
+                return max(piles[i] + dp(i+1, j), piles[j] + dp(i, j-1))
+            else:
+                return min(-piles[i] + dp(i+1, j), -piles[j] + dp(i, j-1))
+        
+        return dp(0, n-1 ) >0
+
+
+        
